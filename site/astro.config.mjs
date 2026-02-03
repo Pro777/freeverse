b0522db97540c96bf259076b27c892e31915b34a
John Malone <john.malone@gmail.com>
2026-02-03 15:50:04 -0600

    Custom domain: thefreeverse.org + update OG image

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pro777.github.io/freeverse/',
-  base: '/freeverse',
+  site: 'https://thefreeverse.org/',
 });
